0c18add89a8f5692cd18a618fb7e37299e226441
Alvin Laia <114079285+alvinlaia11@users.noreply.github.com>
2023-10-16 14:37:06 +0700

    Update Lat17,.py

--- Lat17,.py
+++ Lat17,.py
@@ -1,93 +1,158 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:basic/components/htpHelper.dart';
-import 'package:basic/components/detail.dart';
+import 'package:tmdblistviewtraining/detailscreen.dart';
+import 'package:tmdblistviewtraining/helper.dart';
+import 'package:tmdblistviewtraining/search.dart';
+
+enum MovieCategory { latest, nowPlaying, popular, topRated, upcoming }
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  MovieCategory _selectedCategory = MovieCategory.nowPlaying;
+  StreamController<List?> streamController = StreamController<List?>();
+
   HttpHelper? helper;
   List? movies;
   final String iconBase = 'https://image.tmdb.org/t/p/w92/';
   final String defaultImage =
-      'https://images.freeimages.com/images/large-previews/5eb/movieclapboard-1184339.jpg';
+      'https://images.freeimages.com/images/large-previews/5eb/movie-clapboard-1184339.jpg';
 
-  String selectedGenre = 'All'; // Default selected genre
+  void initialize() {
+    helper?.getMovies(_selectedCategory).listen((movies) {
+      streamController.add(movies);
+    });
+  }
 
   @override
   void initState() {
-    super.initState();
     helper = HttpHelper();
-    initialize();
+    Timer.periodic(Duration(seconds: 5), (Timer t) => initialize());
+    super.initState();
   }
 
-  Future initialize() async {
-    movies = await helper?.getMovies();
-    setState(() {
-      movies = movies;
-    });
+  @override
+  void dispose() {
+    streamController.close();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     NetworkImage image;
+    String getAppBarTitle(MovieCategory category) {
+      switch (category) {
+        case MovieCategory.latest:
+          return 'Latest';
+        case MovieCategory.nowPlaying:
+          return 'Now Playing';
+        case MovieCategory.popular:
+          return 'Popular';
+        case MovieCategory.topRated:
+          return 'Top Rated';
+        case MovieCategory.upcoming:
+          return 'Up Coming';
+        default:
+          return '';
+      }
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Now Playing'),
+        title: Text(getAppBarTitle(_selectedCategory)),
         actions: <Widget>[
-          DropdownButton<String>(
-            value: selectedGenre,
-            items: <String>['All', 'Horror', 'Romantis', 'Comedy']
-                .map((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
-              );
-            }).toList(),
-            onChanged: (String? newValue) {
-              setState(() {
-                selectedGenre = newValue!;
-              });
+          IconButton(
+            onPressed: () {
+              MaterialPageRoute route =
+                  MaterialPageRoute(builder: (_) => const SearchMovie());
+              Navigator.push(context, route);
             },
+            icon: Icon(Icons.search),
           ),
+          PopupMenuButton<MovieCategory>(
+              onSelected: (MovieCategory result) {
+                setState(() {
+                  _selectedCategory = result;
+                  initialize();
+                });
+              },
+              itemBuilder: (BuildContext context) =>
+                  <PopupMenuEntry<MovieCategory>>[
+                    const PopupMenuItem(
+                      value: MovieCategory.latest,
+                      child: Text('Latest'),
+                    ),
+                    const PopupMenuItem(
+                      value: MovieCategory.nowPlaying,
+                      child: Text('Now Playing'),
+                    ),
+                    const PopupMenuItem(
+                      value: MovieCategory.popular,
+                      child: Text('Popular'),
+                    ),
+                    const PopupMenuItem(
+                      value: MovieCategory.topRated,
+                      child: Text('Top Rated'),
+                    ),
+                    const PopupMenuItem(
+                      value: MovieCategory.upcoming,
+                      child: Text('Up Coming'),
+                    ),
+                  ]),
         ],
       ),
-      body: ListView.builder(
-        itemCount: (movies?.length == null) ? 0 : movies?.length,
-        itemBuilder: (BuildContext context, int position) {
-          if (movies![position].posterPath != null) {
-            image = NetworkImage(iconBase + movies![position].posterPath);
-          } else {
-            image = NetworkImage(defaultImage);
+      body: StreamBuilder<List?>(
+        stream: streamController.stream,
+        builder: (BuildContext context, AsyncSnapshot<List?> snapshot) {
+          if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
           }
-
-          if (selectedGenre == 'All' ||
-              movies![position].genre == selectedGenre) {
-            return Card(
-              color: Colors.white,
-              elevation: 2.0,
-              child: ListTile(
-                onTap: () {
-                  MaterialPageRoute route = MaterialPageRoute(
-                      builder: (_) => DetailScreen(movies![position]));
-                  Navigator.push(context, route);
-                },
-                leading: CircleAvatar(
-                  backgroundImage: image,
-                ),
-                title: Text(movies![position].title),
-                subtitle: Text('Released: ' +
-                    movies![position].releaseDate +
-                    ' - Vote: ' +
-                    movies![position].voteAverage.toString()),
-              ),
-            );
-          } else {
-            return SizedBox.shrink();
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return Center(child: CircularProgressIndicator());
+            default:
+              if (snapshot.hasData) {
+                return ListView.builder(
+                  itemCount: snapshot.data?.length ?? 0,
+                  itemBuilder: (BuildContext context, int position) {
+                    ImageProvider image;
+                    if (snapshot.data![position].posterPath != null) {
+                      image = NetworkImage(
+                          iconBase + snapshot.data![position].posterPath);
+                    } else {
+                      image = NetworkImage(defaultImage);
+                    }
+                    return Card(
+                      color: Colors.white,
+                      elevation: 2.0,
+                      child: ListTile(
+                        onTap: () {
+                          MaterialPageRoute route = MaterialPageRoute(
+                              builder: (_) =>
+                                  DetailScreen(snapshot.data![position]));
+                          Navigator.push(context, route);
+                        },
+                        leading: CircleAvatar(
+                          backgroundImage: image,
+                        ),
+                        title: Text(snapshot.data![position].title),
+                        subtitle: Text('Released: ' +
+                            snapshot.data![position].releaseDate +
+                            ' - Vote: ' +
+                            snapshot.data![position].voteAverage.toString()),
+                      ),
+                    );
+                  },
+                );
+              } else {
+                return Text('No data');
+              }
           }
         },
       ),
