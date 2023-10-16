6aff913e2029b5fe3ab38ef3e846023d317973d2
Alvin Laia <114079285+alvinlaia11@users.noreply.github.com>
2023-10-16 14:55:19 +0700

    Update Lat17,.py

--- Lat17,.py
+++ Lat17,.py
@@ -1,65 +1,150 @@
-import 'dart:convert';
-import 'dart:io';
-import 'package:http/http.dart' as http;
-import 'package:tmdblistviewtraining/screen.dart';
+import 'package:flutter/material.dart';
+import 'package:tmdblistviewtraining/detailscreen.dart';
+import 'package:tmdblistviewtraining/helper.dart';
+import 'package:tmdblistviewtraining/search.dart';
 
-class Movie {
-  final int id;
-  final String title;
-  final double voteAverage;
-  final String releaseDate;
-  final String overview;
-  final String posterPath;
+enum MovieCategory { latest, nowPlaying, popular, topRated, upcoming }
 
-  Movie(this.id, this.title, this.voteAverage, this.releaseDate, this.overview,
-      this.posterPath);
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
-  factory Movie.fromJson(Map<String, dynamic> parsedJson) {
-    final id = parsedJson['id'] as int;
-    final title = parsedJson['title'] as String;
-    final voteAverage = parsedJson['vote_average'] * 1.0 as double;
-    final releaseDate = parsedJson['release_date'] as String;
-    final overview = parsedJson['overview'] as String;
-    final posterPath = parsedJson['poster_path'] as String;
-
-    return Movie(id, title, voteAverage, releaseDate, overview, posterPath);
-  }
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class HttpHelper {
-  final String _urlKey = "?api_key=fd32ea8d9e92f032a1073ac1418330b9";
-  final String _urlBase = "https://api.themoviedb.org/";
+class _HomeScreenState extends State<HomeScreen> {
+  MovieCategory _selectedCategory = MovieCategory.nowPlaying;
+  StreamController<List?> streamController = StreamController<List?>();
 
-  Stream<List?> getMovies(MovieCategory category) async* {
-    String? categoryStr; // Menggunakan categoryStr sebagai nullable
-    switch (category) {
-      case MovieCategory.latest:
-        categoryStr = 'upcoming'; //latest but error 401 api
-        break;
-      case MovieCategory.nowPlaying:
-        categoryStr = 'now_playing';
-        break;
-      case MovieCategory.popular:
-        categoryStr = 'popular';
-        break;
-      case MovieCategory.topRated:
-        categoryStr = 'top_rated';
-        break;
-      case MovieCategory.upcoming:
-        categoryStr = 'upcoming';
-        break;
+  HttpHelper? helper;
+  List? movies;
+  final String iconBase = 'https://image.tmdb.org/t/p/w92/';
+  final String defaultImage =
+      'https://images.freeimages.com/images/large-previews/5eb/movie-clapboard-1184339.jpg';
+
+  void initialize() {
+    helper?.getMovies(_selectedCategory).listen((movies) {
+      streamController.add(movies);
+    });
+  }
+
+  @override
+  void initState() {
+    helper = HttpHelper();
+    Timer.periodic(Duration(seconds: 5), (Timer t) => initialize());
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    streamController.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    NetworkImage image;
+    String getAppBarTitle(MovieCategory category) {
+      switch (category) {
+        case MovieCategory.latest:
+          return 'Latest';
+        case MovieCategory.nowPlaying:
+          return 'Now Playing';
+        case MovieCategory.popular:
+          return 'Popular';
+        case MovieCategory.topRated:
+          return 'Top Rated';
+        case MovieCategory.upcoming:
+          return 'Up Coming';
+      }
+      return '';
     }
-    var url = Uri.parse(_urlBase + '/3/movie/' + categoryStr! + _urlKey);
-    http.Response result = await http.get(url);
-    if (result.statusCode == HttpStatus.ok) {
-      final jsonResponse = json.decode(result.body);
-      final moviesMap = jsonResponse['results'];
-      List movies = moviesMap.map((i) => Movie.fromJson(i)).toList();
-      yield movies;
-    } else if (result.statusCode == HttpStatus.unauthorized) {
-      throw Exception('API Telah Mengalami Masalah Silahkan cek kembali');
-    } else {
-      yield null;
-    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(getAppBarTitle(_selectedCategory)),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {
+              MaterialPageRoute route =
+                  MaterialPageRoute(builder: (_) => const SearchMovie());
+              Navigator.push(context, route);
+            },
+            icon: Icon(Icons.search),
+          ),
+        ],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          DropdownButton<MovieCategory>(
+            value: _selectedCategory,
+            onChanged: (MovieCategory newValue) {
+              setState(() {
+                _selectedCategory = newValue;
+                initialize();
+              });
+            },
+            items: MovieCategory.values
+                .where((category) => category != MovieCategory.horror && category != MovieCategory.romance && category != MovieCategory.comedy) // Hapus kategori yang tidak diinginkan
+                .map((category) {
+              return DropdownMenuItem<MovieCategory>(
+                value: category,
+                child: Text(getAppBarTitle(category)),
+              );
+            }).toList(),
+          ),
+          StreamBuilder<List?>(
+            stream: streamController.stream,
+            builder: (BuildContext context, AsyncSnapshot<List?> snapshot) {
+              if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              }
+              switch (snapshot.connectionState) {
+                case ConnectionState.waiting:
+                  return Center(child: CircularProgressIndicator());
+                default:
+                  if (snapshot.hasData) {
+                    return ListView.builder(
+                      itemCount: snapshot.data?.length ?? 0,
+                      itemBuilder: (BuildContext context, int position) {
+                        ImageProvider image;
+                        if (snapshot.data![position].posterPath != null) {
+                          image = NetworkImage(
+                              iconBase + snapshot.data![position].posterPath);
+                        } else {
+                          image = NetworkImage(defaultImage);
+                        }
+                        return Card(
+                          color: Colors.white,
+                          elevation: 2.0,
+                          child: ListTile(
+                            onTap: () {
+                              MaterialPageRoute route = MaterialPageRoute(
+                                  builder: (_) =>
+                                      DetailScreen(snapshot.data![position]));
+                              Navigator.push(context, route);
+                            },
+                            leading: CircleAvatar(
+                              backgroundImage: image,
+                            ),
+                            title: Text(snapshot.data![position].title),
+                            subtitle: Text('Released: ' +
+                                snapshot.data![position].releaseDate +
+                                ' - Vote: ' +
+                                snapshot.data![position].voteAverage.toString()),
+                          ),
+                        );
+                      },
+                    );
+                  } else {
+                    return Text('No data');
+                  }
+              }
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
